7360cb5fb3e89c29537d30be46958f1b4ea064ea
Sara Wachulec <sara.wachulec@relativity.com>
2022-07-29 10:47:39 +0200

    Live program and program to display colors

---
+++ lab01_colors/Program.cs
@@ -0,0 +1,9 @@
+﻿//All console colors
+Console.WriteLine("========COLORS========");  
+foreach(ConsoleColor color in Enum.GetValues(typeof(ConsoleColor))) {  
+    Console.ForegroundColor = color;
+    Console.Write($"Color is set to {color}");  
+    Console.ResetColor();
+    Console.WriteLine();
+}
+
---
+++ lab01_live/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+// ghhkjsdhgkjshdgjkhsd
+/*
+koentarz 
+na wlebnjsxfskhgk
+*/
+
+//var name = "Adaś";
+
+Console.WriteLine("Jak masz na imię?");
+var name = Console.ReadLine();
+
+
+if(name == "Sara")
+{
+    Console.WriteLine("A to ty, "+ name);
+} 
+else if (name == "Adam")
+{
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.BackgroundColor = ConsoleColor.Red;
+    for(var i=0;i<10;i++){
+        Console.Write("AAAA ");
+        Console.WriteLine(name + " ma sami wiecie co");
+    }
+    Console.ResetColor();
+}
+else 
+{
+    Console.WriteLine("Witaj " + name);
+}
+
